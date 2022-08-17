445b8cd504f825320ff6d9fddf42054e0c46a33e
bruk19 <brukteshome21@gmail.com>
2022-08-17 14:28:30 +0300

    Add required changes

--- schema.sql
+++ schema.sql
@@ -1,12 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-CREATE TABLE animals (
 CREATE TABLE animals(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(100)
-    id INT,
     name VARCHAR,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL
-);
 );
