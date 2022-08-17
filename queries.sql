f3b162821133bef3d215a1ca529a6730e3d44102
bruk19 <brukteshome21@gmail.com>
2022-08-17 13:26:54 +0300

    Add required changes

--- queries.sql
+++ queries.sql
@@ -1,15 +1,14 @@
 /*Queries that provide answers to the questions from all projects.*/
-select *
-from animals
+SELECT *from animals
 WHERE name LIKE '%mon';
 
 -- List the name of all animals born between 2016 and 2019.
-select * from animals
+SELECT * from animals
 WHERE date_of_birth 
 BETWEEN '01/01/2016' AND '31/12/2019';
 
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
-select name from animals
+SELECT name from animals
 WHERE neutered=TRUE 
 AND escape_attempts < 3;
 
@@ -24,17 +23,14 @@ FROM animals
 WHERE weight_kg > 10.5;
 
 -- Find all animals that are neutered.
-SELECT *
-FROM animals
+SELECT *FROM animals
 WHERE neutered=TRUE;
 
 -- Find all animals not named Gabumon.
-SELECT *
-FROM animals
+SELECT *FROM animals
 WHERE name!='Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
-SELECT *
-FROM animals
+SELECT *FROM animals
 WHERE weight_kg 
 BETWEEN 10.4 AND 17.3;
