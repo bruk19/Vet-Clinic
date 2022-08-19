3d4472d5833fcaefe5c1f01e526509fd47d16033
bruk19 <brukteshome21@gmail.com>
2022-08-19 12:14:46 +0300

    Update inserted animals to include owner information

--- data.sql
+++ data.sql
@@ -65,3 +65,10 @@ WHERE name LIKE '%mon';
 UPDATE animals
 SET species_id = 1
 WHERE species_id IS NULL;
+
+--Update inserted animals to include owner information(owner_id)
+UPDATE animals SET owner_id = 1 WHERE name IN ('Agumon');
+UPDATE animals SET owner_id = 2 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle','Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
